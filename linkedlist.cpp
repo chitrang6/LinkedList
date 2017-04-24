b49651d8ca699dd867559fca0ab2fa8e484ce527
Chitrang <chitrang6@gmail.com>
2017-04-23 21:52:46 -0700

    added the function to split the LinkedList into two halves.

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -26,7 +26,7 @@ node* createNode(int key)
 {
 	node* nodePtr = (node *) malloc(sizeof(node));
 	nodePtr->data = key;
-	nodePtr->next = NULL;
+	nodePtr->next = nullptr;
 	return nodePtr;
 }
 
@@ -38,7 +38,7 @@ node* push(node* headRef, int key)
 
 	node* temp = createNode(key);
 
-	if(headRef == NULL)
+	if(headRef == nullptr)
 	{	
 		headRef = temp;
 		return headRef;
@@ -75,7 +75,7 @@ void printList(node* headRef)
 {
 	node* cur = headRef;
 	cout<<"HEAD -> ";
-	while(cur != NULL)
+	while(cur != nullptr)
 	{
 		cout<<cur->data<<"->";
 		cur = cur->next;
@@ -127,10 +127,10 @@ node* deleteNode(node* headRef, int position)
 node* reverse_iterative(node* headRef)
 {
 
-	node* prev = NULL;
+	node* prev = nullptr;
 	node* next_node;
 	node* current = headRef;
-	while(current != NULL)
+	while(current != nullptr)
 	{
 		next_node = current->next;
 		current->next = prev;
@@ -166,6 +166,107 @@ node* reverseRecursion(node* headRef)
 
 */
 
+// Find the length of the linked list 
+
+
+int getLength(node* headRef)
+{
+
+	int len = 0;
+	if(headRef == nullptr)
+	{
+		return 0;
+	}
+
+
+	else
+	{
+		node* current = headRef;
+		while(current != nullptr)
+		{
+			current = current->next;
+			len++;
+		}
+
+	return len;
+	}
+}
+
+
+
+
+// Now Lets split my lined List in two halves.
+
+
+void SplitIntoTwoHalvesMethod1(node* headRef, node** first, node** second)
+{
+
+
+	// First let handle the case when the length of the list is less than  2.
+	int len = getLength(headRef);
+
+	if(len < 2)
+	{
+		*first = headRef;
+		*second = nullptr; 
+	}
+
+	else
+	{
+		node* current = headRef;
+		int toReach = (len - 1) / 2;
+		for(int i = 0 ; i < toReach ; i++)
+		{
+			current = current->next;
+		}
+
+		*second = current->next;
+		*first = headRef;
+		current->next = nullptr;
+
+	}
+
+}
+
+
+void SplitIntoTwoHalvesMethod2(node* headRef, node** first, node** second)
+{
+
+	if(headRef == nullptr | headRef->next == nullptr)
+	{
+		*first = headRef;
+		*second = nullptr; 
+	}
+
+	else
+	{
+		// Make two poinnters to traverse the linked list
+
+		node* slow = headRef;
+		node* fast = headRef;
+
+		while(slow && fast && fast->next)
+		{
+			slow = slow->next;
+			fast = fast->next->next;
+		}
+		// Here slow pointer will be at the mid point.
+
+		*second = slow->next;
+		*first = headRef;
+		slow->next = nullptr;
+
+	}
+
+
+}
+
+
+
+
+
+
+
 
 int main()
 {
@@ -175,8 +276,8 @@ int main()
 	// So every method we will write will going to return the head pointer to update the head pointer.
 
 
-	int data[10] = {12 , 21 , 2 , 23 ,234  , 123  , 234 , 23 , 1234 , 124};
-	head = CreateList(head, data, 10);
+	int data[8] = {12 , 21 , 2 , 23 ,234  , 123  , 234 , 23 };
+	head = CreateList(head, data, 8);
 	printList(head);
 
 	head = deleteNode(head , 6);
@@ -186,6 +287,21 @@ int main()
 	head = reverse_iterative(head);
 
 	printList(head);
+
+
+	node* firstHalf = nullptr;
+	node* secondHalf = nullptr;
+
+
+	cout<<"Spliting the LinkedList"<<endl;
+
+	SplitIntoTwoHalvesMethod2(head , &firstHalf, &secondHalf);
+
+
+	printList(firstHalf);
+
+	printList(secondHalf);
+
 
 }
 
